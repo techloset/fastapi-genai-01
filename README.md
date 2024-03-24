e03b1ef195d0f51ea670faf424894b3e00d96acf
naveed-rana <rana.naveed812@gmail.com>
2024-03-24 12:03:24 +0500

    feat: student app concept added

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
  poetry run dev
 
 ```
+
+Steps1 : 
+- curl -sSL https://install.python-poetry.org | python -
+
+- set env variable from controll panel 
+  C:\Users<user>\AppData\Roaming\Python\Scripts
+
+- poetry add fastapi "uvicorn["standard"]"
+
+- poetry run dev
+
+
+
+
--- todos/main.py
+++ todos/main.py
@@ -3,23 +3,43 @@ import uvicorn
 
 app = FastAPI()
 
+students = [{
+    "userName":"Ali",
+    "rollNo": 2342
+},
+            {
+    "userName":"Naveed Sarwar",
+    "rollNo": 9213
+}
+            ]
+
+@app.get("/students")
+def getStudents():
+    return students
+
+@app.get("/addStudent")
+def addStudent(userName:str, rollNo:str):
+    global students
+    students.append({"userName":userName, "rollNo":rollNo})
+    return students
+
 @app.get("/")
 def helloWorld():
-    return "Hello, world!"
+    return {"hello": "world"}
 
-@app.get("/gettodos")
-def getTodos():
-    print("Get todos called")
-    return "gettodos called"
+@app.get("/gettodos/{userName}/{rollNo}")
+def getTodos(userName:str, rollNo:str):
+    print("Get todos called",userName,rollNo)
+    return userName + rollNo 
 
 @app.post("/gettodos")
 def getTodosPost():
     print("Get post method todos called")
     return "post gettodos called"
 
-@app.get("/getSignleTodo")
-def getSingleTodo():
-    print("Get todo called")
+@app.get("/getSingleTodo")
+def getSingleTodo(userName:str, rollNo:str):
+    print("Get todo called",userName,rollNo )
     return "getSingleTodo called sadf sd sdff"
 
 @app.put("/updateTodo")
@@ -29,5 +49,7 @@ def updateTodo():
 
 def start():
     uvicorn.run("todos.main:app",host="127.0.0.1", port=8080, reload=True)
+
+
 
 
